06d560cc39695ab6daa3d66bd5cfbd13ec78e1a6
nameless-newbie <siagaws@gmail.com>
2026-01-22 03:21:31 +0700

    3, 2, 1, and ready to go!

--- setup.sh
+++ setup.sh
@@ -211,7 +211,7 @@ function is_root() {
 # Buat direktori xray
 print_install "Membuat direktori xray"
     mkdir -p /etc/xray
-    curl -s ifconfig.me > /etc/xray/ipvps
+    curl -s icanhazip.com > /etc/xray/ipvps
     touch /etc/xray/domain
     mkdir -p /var/log/xray
     chown www-data.www-data /var/log/xray
